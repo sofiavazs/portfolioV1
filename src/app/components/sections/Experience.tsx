ef1325ac08b32318e31da28647c5e9270026b746
Sofia <sofia.sousa@chorus.se>
2025-04-01 23:13:35 +0100

    chore: update resume + experience

--- src/app/components/sections/Experience.tsx
+++ src/app/components/sections/Experience.tsx
@@ -1,10 +1,9 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import styled from "styled-components";
 import experience from "../../api/experience.json";
 import Section from "../Section";
 import Link from "next/link";
-
 
 const Experience: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -22,16 +21,16 @@ const Experience: React.FC = () => {
                   <p className="subheading">{job.title}</p>
                   <p>{job.description}</p>
                 </CardHeader>
-                {hasProjects &&
+                {hasProjects && (
                   <Button onClick={() => setIsOpen(!isOpen)}>
                     {!isOpen ? "View Projects" : "Close"}
                   </Button>
-                }
+                )}
                 <hr />
                 {isOpen && (
                   <CardBody>
                     <>
-                      {job.projects.map((project, i) => (
+                      {job.projects?.map((project, i) => (
                         <div key={i}>
                           <h4>{project.name}</h4>
                           <p className="project-subheader">{project.dates}</p>
@@ -43,13 +42,14 @@ const Experience: React.FC = () => {
                   </CardBody>
                 )}
               </Card>
-            )
+            );
           })}
           <Link
             href="/resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
-            className="resume-link">
+            className="resume-link"
+          >
             View full resume
           </Link>
         </Wrapper>
@@ -60,7 +60,7 @@ const Experience: React.FC = () => {
 export default Experience;
 
 const Wrapper = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -70,17 +70,18 @@ const Wrapper = styled.div`
     width: fit-content;
     padding: 10px;
     margin-right: 30px;
-    border: 1px solid ${props => props.theme.tertiaryColor};
+    border: 1px solid ${(props) => props.theme.tertiaryColor};
     border-radius: 4px;
     background: transparent;
     cursor: pointer;
-    color:${props => props.theme.tertiaryColor};
+    color: ${(props) => props.theme.tertiaryColor};
     font-size: 1rem;
     font-family: monospace;
-    box-shadow: 3px 3px 0 0 ${props => props.theme.tertiaryColor};
+    box-shadow: 3px 3px 0 0 ${(props) => props.theme.tertiaryColor};
     text-decoration: none;
 
-    &:hover, :active {
+    &:hover,
+    :active {
       box-shadow: none;
       transform: translate(4px, 4px);
     }
@@ -94,15 +95,15 @@ const Card = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background: ${props => props.theme.card.background};
+  background: ${(props) => props.theme.card.background};
   border-radius: 16px;
-  box-shadow: ${props => props.theme.card.boxShadow};
+  box-shadow: ${(props) => props.theme.card.boxShadow};
   backdrop-filter: blur(6.5px);
   -webkit-backdrop-filter: blur(6.5px);
-  border: ${props => props.theme.card.border};
+  border: ${(props) => props.theme.card.border};
 
   h3 {
-    color: ${props => props.theme.primaryColor};
+    color: ${(props) => props.theme.primaryColor};
     font-size: 2rem;
     margin: 0;
   }
@@ -112,7 +113,12 @@ const Card = styled.div`
     border: 0;
     height: 1px;
     margin: 1rem;
-    background-image: linear-gradient( to right, rgba(249, 247, 247, 0), rgba(58, 10, 146, 0.402), rgba(237, 237, 237, 0));
+    background-image: linear-gradient(
+      to right,
+      rgba(249, 247, 247, 0),
+      rgba(58, 10, 146, 0.402),
+      rgba(237, 237, 237, 0)
+    );
   }
 
   button {
@@ -125,7 +131,7 @@ const CardHeader = styled.div`
   flex-direction: column;
   word-wrap: break-word;
   p {
-    color:  ${props => props.theme.primaryColor};
+    color: ${(props) => props.theme.primaryColor};
     font-size: 1rem;
     line-height: 1.75rem;
   }
@@ -141,12 +147,12 @@ const CardHeader = styled.div`
 
 const CardBody = styled.div`
   h4 {
-    color:  ${props => props.theme.primaryColor};
+    color: ${(props) => props.theme.primaryColor};
     font-size: 1.5rem;
     margin-bottom: 0;
   }
   p {
-    color:  ${props => props.theme.primaryColor};
+    color: ${(props) => props.theme.primaryColor};
     font-size: 1rem;
     line-height: 1.75rem;
   }
@@ -164,18 +170,19 @@ const Button = styled.button`
   width: fit-content;
   padding: 10px;
   background: transparent;
-  border: 1px solid  ${props => props.theme.tertiaryColor};
+  border: 1px solid ${(props) => props.theme.tertiaryColor};
   cursor: pointer;
   border-radius: 4px;
-  color: ${props => props.theme.tertiaryColor};
+  color: ${(props) => props.theme.tertiaryColor};
   font-size: 1rem;
   font-family: monospace;
   text-align: center;
-  box-shadow: 3px 3px 0 0 ${props => props.theme.tertiaryColor};
+  box-shadow: 3px 3px 0 0 ${(props) => props.theme.tertiaryColor};
 
-  &:hover, :active {
+  &:hover,
+  :active {
     box-shadow: none;
     transform: translate(4px, 4px);
-    transition: all .3s ease;
+    transition: all 0.3s ease;
   }
 `;
