8b839ef79e24f4b1c1aef452371df454a43c3d57
sofiavazsousa <sofiavazsousa@gmail.com>
2025-10-29 23:46:39 +0000

    styling: add prop to allow section to be full width

--- src/app/components/ui/Section.tsx
+++ src/app/components/ui/Section.tsx
@@ -1,17 +1,23 @@
 "use client";
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface SectionProps {
   sectionId?: string;
   header?: string;
   children?: React.ReactNode;
+  fullWidth?: boolean;
 }
 
-const Section: React.FC<SectionProps> = ({ sectionId, header, children }) => {
+const Section: React.FC<SectionProps> = ({
+  sectionId,
+  header,
+  children,
+  fullWidth,
+}) => {
   return (
     <>
-      <Container id={sectionId}>
+      <Container id={sectionId} $fullWidth={fullWidth}>
         <h2>{header}</h2>
         {children}
       </Container>
@@ -20,11 +26,19 @@ const Section: React.FC<SectionProps> = ({ sectionId, header, children }) => {
 };
 export default Section;
 
-const Container = styled.section`
+const Container = styled.section<{ $fullWidth?: boolean }>`
   width: 100%;
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
+  max-width: 1400px;
+  margin: 0 auto;
+
+  ${({ $fullWidth }) =>
+    $fullWidth &&
+    css`
+      max-width: 100%;
+    `}
 
   h2 {
     font-size: 8rem;
